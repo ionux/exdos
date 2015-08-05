d570db860b7c837fee5df78a3e4d21ab3874b18c
Omar Mohammad <omarx024@gmail.com>
2015-08-05 20:07:36 +0300

    Create root.asm

---
+++ tmp/root.asm
@@ -0,0 +1,33 @@
+use32
+
+filename	db			"kernel  sys"
+lba		dd			200
+size_sectors	dd			80
+size_bytes	dd			12288
+time		db			11		; hour
+		db			36		; minute
+date		db 			20		; day
+		db			6		; month
+		dw 			2015		; year
+
+		db			"init    exe"
+		dd			700
+		dd			8
+		dd			16384
+		db			11		; hour
+		db			36		; minute
+		db 			20		; day
+		db			6		; month
+		dw 			2015		; year
+
+		db			"hello   exe"
+		dd			800
+		dd			1
+		dd			16384
+		db			11		; hour
+		db			36		; minute
+		db 			20		; day
+		db			6		; month
+		dw 			2015		; year
+
+
